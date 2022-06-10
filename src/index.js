3b952cc9306c3852c10811ef1885dbfebda198ac
dome4 <dominic.seitz@gmx.de>
2022-06-10 11:03:14 +0200

    add cron job overlap logic

--- src/index.js
+++ src/index.js
@@ -4,34 +4,40 @@ const { spawn } = require("child_process");
 const SCHEDULE = process.env.SCHEDULE || "0 0 * * *";
 const TIME_ZONE = process.env.TIME_ZONE || "Europe/London";
 
-function executeRenovate() {
-    const renovate = spawn("docker-entrypoint.sh", {
-        cwd: '/usr/src/app',
-        stdio: "inherit",
-        stderr: "inherit",
-    });
-    renovate.on('close', (code) => {
-        if (code !== 0) {
+function executeRenovate(job) {
+
+    // prevent cron job overlap
+    if(!job.running){
+        const renovate = spawn("docker-entrypoint.sh", {
+            cwd: '/usr/src/app',
+            stdio: "inherit",
+            stderr: "inherit",
+        });
+        renovate.on('close', (code) => {
+            if (code !== 0) {
+                job.stop();
+                console.error('Failed in renovate. Return code', code);
+                process.exit(code);
+            }
+            console.log(`Renovate exited with code ${code}`);
+        });
+        renovate.on('error', (err) => {            
             job.stop();
-            console.error('Failed in renovate. Return code', code);
-            process.exit(code);
-        }
-        console.log(`Renovate exited with code ${code}`);
-    });
-    renovate.on('error', (err) => {
-        job.stop();
-        console.error('Failed in renovate. Return code');
-        console.error(err);
-        process.exit(1);
-    });
+            console.error('Failed in renovate. Return code');
+            console.error(err);
+            process.exit(1);
+        });
+    } else {
+        console.log(`Renovate is already running`);
+    }
 }
 
 // see https://crontab.guru/
 var job = new CronJob(SCHEDULE, () => {
     console.log('--- CRON JOB CALL ---');
-    executeRenovate();
+    executeRenovate(job);
 },() => {},false, TIME_ZONE);
 console.log('--- CRON JOB START ---');
 console.log(`--- SCHEDULE: ${SCHEDULE} ---`);
 job.start();
-executeRenovate();
+executeRenovate(job);
